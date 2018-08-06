e5c2577872cc0556c0a3e22bfd9163c3b0be10de
Frank Santiesteban <franksantiesteban@nancy-vaio.atnind.local>
2018-08-06 15:09:24 -0400

    connected server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require ('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
