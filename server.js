1bd661884d80630c060e97182b099ebe6ff82b02
Frank Santiesteban <franksantiesteban@yahoo.com>
2018-10-09 20:15:40 -0400

    pushing new repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require ('body-parser');
+var path = require('path');
+
+var PORT = process.env.PORT || 8080;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ type: 'application/*+json'}));
+
+app.use(bodyParser.raw({ type: 'application/vnd.api+json' }));
+
+app.use(bodyParser.text({ type: 'text/html' }));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
